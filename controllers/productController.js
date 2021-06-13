336f0c3d5c6ae3bc908ce7bb44527518ad943978
Ricky Ackerman <rickyackerman.aot@gmail.com>
2021-06-13 15:41:30 +0700

    menambahkan fasilitas CRUD

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,24 @@
 const Product = require('../models/Product')
+const slug = require('slug')
+
+const createProduct = (req, res, next) => {
+    const name = req.body.name
+    const uri = slug(name)
+    const price = req.body.price
+    const stock = req.body.stock
+    const description = req.body.description
+
+    const product = new Product({
+        name: name,
+        slug: uri,
+        price: price,
+        stock: stock,
+        description: description
+    })
+    product.save()
+    res.status(201)
+        .json(product)
+}
 
 const getAllProducts = async (req, res, next) => {
     try {
@@ -10,6 +30,52 @@ const getAllProducts = async (req, res, next) => {
     }
 }
 
+const getSomeProduct = async (req, res, next) => {
+    try {
+        const response = await Product.findOne({slug:req.params.slug})
+        res.status(200)
+            .json(response)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+const deleteProduct = async (req, res, next) => {
+    try {
+        const response = await Product.findOneAndDelete({slug: req.params.slug})
+        res.status(202)
+            .json(response)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+const updateProduct = async (req, res, next) => {
+
+    const name = req.body.name
+    const price = req.body.price
+    const stock = req.body.stock
+    const description = req.body.description
+
+
+    try {
+        const response = await Product.findOneAndUpdate({slug: req.params.slug}, {
+            name: name,
+            price: price,
+            stock: stock,
+            description: description
+        })
+        res.status(202)
+            .json(response)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    createProduct,
+    getSomeProduct,
+    deleteProduct,
+    updateProduct
 }
--- routers/version1.js
+++ routers/version1.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {getAllProducts} = require('../controllers/productController')
+const {getAllProducts, createProduct, getSomeProduct, deleteProduct, updateProduct} = require('../controllers/productController')
 
 router.get('/products', getAllProducts)
+router.get('/products/:slug', getSomeProduct)
+router.post('/products', createProduct)
+router.patch('/products/:slug', updateProduct)
+router.delete('/products/:slug', deleteProduct)
 
 module.exports = router
