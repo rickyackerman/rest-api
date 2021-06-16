054651d96eb9b8ebbdc2cc126fbb79f2c91e25bc
Ricky Ackerman <rickyackerman.aot@gmail.com>
2021-06-17 01:33:40 +0700

    membuat user controller

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+const register = async (req, res, next) => {
+	const response = new User({
+		name: req.body.name,
+		username: req.body.username,
+		email: req.body.email,
+		password: req.body.password,
+		phone: req.body.phone
+	})
+	await response.save()
+	res.status(201)
+		.json(response)
+}
+
+module.exports = {
+	register
+}
